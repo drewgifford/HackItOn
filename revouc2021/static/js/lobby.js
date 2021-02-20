d67c7759f9895b4f7854f61ac98117b6b398df5f
Drew Gifford <legos30000@hotmail.com>
2021-02-20 13:27:56 -0500

    no message

---
+++ revouc2021/static/js/lobby.js
@@ -0,0 +1,21 @@
+const path = "../static";
+
+$(document).load(function(){
+
+    //Check if user is already logged in
+
+    if(false){
+
+    } else {
+        //User is not ingame
+        $("#content").load(`${path}/html/join.html`, function(){
+            
+        });
+    }
+
+
+
+
+
+
+});
